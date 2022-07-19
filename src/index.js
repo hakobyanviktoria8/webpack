cdc9d2da497b2233f4be5a77855d3473b179b848
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-07-20 00:00:44 +0400

    added header classs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import addedImg from "./components/addedImg/add-img"
 import Button from "./components/button/button"
+import Header from "./components/header/header"
 import welcome from "./welcome"
 
 const useBtn = new Button()
+const header = new Header()
+
+header.render()
 addedImg()
 useBtn.render()
 welcome("home")
